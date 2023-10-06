4007f88c9c6f315b152cf42385d6064af963dd7c
Pradnya Narawade <125553998+Pradnya0110@users.noreply.github.com>
2023-10-06 18:26:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from '../MyProject/src/components/Home'
+
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+      <Home/>
+    </View>
+  )
+}
